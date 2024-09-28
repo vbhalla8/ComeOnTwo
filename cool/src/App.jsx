ad2b0d4231ebc7e3ea18fc21ab0c644aa26fd511
Vaishnavi Bhalla <Navibhalla113@gmail.com>
2024-09-27 21:59:37 -0400

    Fix index.html

--- cool/src/App.jsx
+++ cool/src/App.jsx
@@ -5,16 +5,15 @@ import { Hero } from "./components /Hero/Hero";
 
 import { Navbar } from "./components /NavBar /Navbar";
 
+
+
 function App() {
   return (
 
     <div className={styles.App} >
       < Navbar />
       <Hero />
-
-
-
-     
+      
     </div>
   )
 }
